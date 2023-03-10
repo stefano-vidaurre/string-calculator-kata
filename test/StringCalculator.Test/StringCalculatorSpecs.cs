28ab78079f52ba699f76f453c09a238510fe7cdc
Stefano Vidaurre Olite <svidaurre@domingoalonsogroup.com>
2023-03-10 17:29:36 +0100

    Test: Should return two when input is two

--- test/StringCalculator.Test/StringCalculatorSpecs.cs
+++ test/StringCalculator.Test/StringCalculatorSpecs.cs
@@ -14,12 +14,21 @@ public class StringCalculatorSpecs
     }
     
     // TODO: Comprobar casos con un solo numero: 1, 2, 3
+    
     [Test]
     public void ShouldReturnOneWhenInputIsOne()
     {
         int result = Program.Add("1");
         
         result.Should().Be(1);
+    }
+    
+    [Test]
+    public void ShouldReturnTwoWhenInputIsTwo()
+    {
+        int result = Program.Add("2");
+        
+        result.Should().Be(2);
     }
     
     // TODO: Comprobar casos con dos numeros: "4,5", "6,7", "8,9"
