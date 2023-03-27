f4d78caa7f17628bc00a432acd18f672f837e940
Stefano Vidaurre Olite <svidaurre@domingoalonsogroup.com>
2023-03-27 18:07:35 +0200

    refactor: divide into small functions

--- src/StringCalculatorKata.Console/StringCalculator.cs
+++ src/StringCalculatorKata.Console/StringCalculator.cs
@@ -5,20 +5,45 @@ public class StringCalculator
     public int Add(string numbers)
     {
         char[] separators = { ',', '\n' };
-        
+
         if (string.IsNullOrWhiteSpace(numbers))
         {
             return 0;
         }
         
-        if (numbers.StartsWith("//"))
+        if (HasCustomSeparator(numbers))
         {
-            separators = separators.Append(numbers[2]).ToArray();
-            numbers = numbers[4..];
+            return SumWithCustomSeparator(numbers, separators);
         }
 
+        return SumNumbersParsed(ParseNumbers(numbers, separators));
+    }
+
+    private static int SumWithCustomSeparator(string numbers, char[] separators)
+    {
+        separators = GetCustomSeparators(numbers, separators);
+        return SumNumbersParsed(ParseNumbers(numbers[4..], separators));
+    }
+
+    private static int SumNumbersParsed(IEnumerable<int> numbersParsed)
+    {
+        return numbersParsed.Sum();
+    }
+
+    private static IEnumerable<int> ParseNumbers(string numbers, char[] separators)
+    {
         return numbers.Split(separators)
-            .Select(int.Parse)
-            .Sum();
+            .Select(int.Parse);
+    }
+
+    private static char[] GetCustomSeparators(string numbers, char[] separators)
+    {
+        separators = separators.Append(numbers[2]).ToArray();
+        return separators;
+    }
+
+    private static bool HasCustomSeparator(string numbers)
+    {
+        return numbers.StartsWith("//");
     }
 }
